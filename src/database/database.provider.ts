b07a80eddd7050b3ff337b1fd052a8cc8bf72421
DaniloNogueira-Silva <dansilva730x@gmail.com>
2024-05-16 15:50:41 -0300

    feat: removendo conexão com o banco

--- src/database/database.provider.ts
+++ src/database/database.provider.ts
@@ -4,9 +4,9 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 export const databaseProviders = [
-  {
-    provide: 'DATABASE_CONNECTION',
-    useFactory: (): Promise<typeof mongoose> =>
-      mongoose.connect(process.env.DB_URL),
-  },
+  // {
+  //   provide: 'DATABASE_CONNECTION',
+  //   useFactory: (): Promise<typeof mongoose> =>
+  //     mongoose.connect(process.env.DB_URL),
+  // },
 ];
